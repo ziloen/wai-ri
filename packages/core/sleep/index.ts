a28b5ef6fc54807c6d384a1e8d1a9bfc3ad793ee
Neo <neo@neo.neo>
2022-07-10 21:22:57 +0800

    chore

--- packages/core/sleep/index.ts
+++ packages/core/sleep/index.ts
@@ -1,6 +1,6 @@
 import type { Fn } from '@wai-ri/shared'
 
 /** 延迟执行 */
-export function sleep<P extends any[], R = void>(ms?: number, func?: Fn<P, R>, ...args: P): Promise<R> {
-  return new Promise(resolve => setTimeout(() => { resolve(func?.(...args)!) }, ms))
+export async function sleep<P extends any[], R = void>(ms?: number, func?: Fn<P, R>, ...args: P): Promise<R> {
+  return new Promise(resolve => { setTimeout(() => { resolve(func?.(...args) as R) }, ms) })
 }
