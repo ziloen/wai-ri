797f8aca54bf9ce1cbfe9fe456eca373805769eb
Neo <neo@neo.neo>
2022-07-16 19:21:48 +0800

    chore: 改名字

--- packages/shared/type/index.ts
+++ packages/shared/type/index.ts
@@ -163,6 +163,6 @@ export type Extensible<O extends ObjectType> = Expand<O & ObjectType>
 
 
 /** 使两属性互斥 */
-export type MutuallyExclusive<T, K1 extends keyof T, K2 extends keyof T, K extends keyof T = keyof T,> =
+export type Exclusive<T, K1 extends keyof T, K2 extends keyof T, K extends keyof T = keyof T,> =
   ({ [P in Exclude<K, K1>]: T[P] } & { [P in K1]?: never }) |
   ({ [P in Exclude<K, K2>]: T[P] } & { [P in K2]?: never })
