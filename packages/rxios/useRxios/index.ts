904dafbd2415beda0606073e2489a8e4a315c65f
Neo <neo@neo.neo>
2022-08-03 19:01:06 +0800

    chore: useRxios

---
+++ packages/rxios/useRxios/index.ts
@@ -0,0 +1,18 @@
+
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import AxiosStatic from 'axios'
+import { Observable } from 'rxjs'
+
+
+export function useRxios(instance: AxiosInstance) {
+  return function rxios<ResponseData, ConfigData>(config: AxiosRequestConfig<ConfigData>) {
+    return new Observable<AxiosResponse<ResponseData, ConfigData>>(observer => {
+      const controller = new AbortController()
+      instance.request<ResponseData>({ ...config, signal: controller.signal })
+        .then(val => (observer.next(val), observer.complete()))
+        .catch(err => AxiosStatic.isCancel(err) ? observer.complete() : observer.error(err))
+
+      return controller.abort.bind(controller)
+    })
+  }
+}
