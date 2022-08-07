143cde949bd9319f9edb1d2b028caf451590caf5
Neo <neo@neo.neo>
2022-08-07 18:46:54 +0800

    chore: 去除过期注释

--- packages/core/bindSelf/index.ts
+++ packages/core/bindSelf/index.ts
@@ -6,10 +6,3 @@ import { Fn } from '@wai-ri/shared'
 export function bindSelf<T extends Record<any, any>, K extends keyof T>(obj: T, key: T[K] extends Fn ? K : never): T[K] {
   return Function.prototype.bind.call(Reflect.get(obj, key), obj)
 }
-
-
-// 适用情况
-// const obj = new Some()
-// const fn = obj.fn.bind(obj)
-// 简写成一行
-// const fn = bindSelf(new Some(), 'fn')
