08d4fc8bf6f478564f0b19b70acddf384f794616
Neo <neo@neo.neo>
2022-08-09 19:50:56 +0800

    chore: test

--- packages/core/useEnum/index.test.ts
+++ packages/core/useEnum/index.test.ts
@@ -2,25 +2,25 @@ import { useEnum } from '.'
 
 
 describe("useEnum", () => {
-  
+
   it('双向绑定', () => {
     const e = useEnum({ a: 12, b: '9', c: false, d: undefined, e: null } as const)
     expect(e.a).toBe(12)
     expect(e[12]).toBe('a')
-    
+
     expect(e.b).toBe('9')
     expect(e[9]).toBe('b')
-    
+
     expect(e.c).toBe(false)
     expect(e.false).toBe('c')
-    
+
     expect(e.d).toBe(undefined)
     expect(e.undefined).toBe('d')
-    
+
     expect(e.e).toBe(null)
     expect(e.null).toBe('e')
   })
-  
+
   it('for..of循环', () => {
     const e = useEnum({ a: 12, b: '9', c: false, d: undefined, e: null } as const)
     const keys = []
@@ -29,16 +29,16 @@ describe("useEnum", () => {
       keys.push(key)
       vals.push(val)
     }
-    
+
     expect(keys.length).toBe(5)
   })
-  
+
   it('for..of 不受源对象影响', () => {
     const a = { a: 12, b: '9', c: false, d: undefined, e: null }
     const e = useEnum(a)
 
     a.a = 21
-    for(const [key, val] of e) {
+    for (const [key, val] of e) {
       expect(e[key]).toBe(val)
     }
   })
