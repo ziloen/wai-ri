fa0727025a9662546b3486d71adca040de2f40f5
zhupengcheng <zhupengcheng5@163.com>
2022-08-17 11:58:27 +0800

    chore: upadte typescript

---
+++ packages/core/useClipboard/index.ts
@@ -0,0 +1,24 @@
+
+
+/** 读取或写入剪切板 */
+export async function useClipboard(): Promise<any>
+export async function useClipboard(value: any): Promise<any>
+export async function useClipboard(...args: any[]): Promise<any> {
+  if (!(navigator && 'permissions' in navigator && 'clipboard' in navigator)) return Promise.reject(new Error('[useClipboard]: Clipboard is not supported!'))
+
+  const value = args[0]
+  
+  if (args.length !== 0) {
+    type N = PermissionDescriptor
+    // const permissions = navigator.permissions.query('clip')
+    return navigator.clipboard.writeText(value)
+  }
+  
+  else {
+    navigator.permissions.query({ name: 'clipboard-read' as any })
+      .then(status => {
+        status.state
+      })
+    navigator.clipboard.readText()
+  }
+}
