40682ea7b3544e2e581a8805dbfe91d5b54ea9a9
zhupengcheng <zhupengcheng5@163.com>
2022-08-19 14:52:56 +0800

    chore

--- packages/core/match/index.ts
+++ packages/core/match/index.ts
@@ -1,13 +1,16 @@
 
 
+const FALLBACK_ARM = '_'
 
 type MatchArm = {
   [key: keyof any]: () => any
-  // _ : () => any
 }
 
-/** 模式匹配 */
+/** 
+ * 模式匹配  
+ * {@link FALLBACK_ARM default 分支}
+ */
 export function match<T>(value: T, matchArms: MatchArm) {
-  const matched = matchArms[value as keyof MatchArm] ?? matchArms['_']
-  return matched()
+  const resultFn = matchArms[value as keyof MatchArm] ?? matchArms[FALLBACK_ARM]
+  return resultFn()
 }
