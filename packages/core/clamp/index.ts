e948b58d45e51c0520594c9f0da89ab2bf1ac668
zhupengcheng <zhupengcheng5@163.com>
2022-08-19 17:50:20 +0800

    chore

--- packages/core/clamp/index.ts
+++ packages/core/clamp/index.ts
@@ -1,26 +1,22 @@
 import { isNil } from '@wai-ri/shared'
 
 
+
 type MaybeNumber = number | undefined | null
+
 /** 限定数值大小 */
-export function clamp(n: number, min: number, max: number): number
-export function clamp(n: MaybeNumber[], min: number, max: number): MaybeNumber[]
-export function clamp(n: number | MaybeNumber[], min: number, max: number): number | MaybeNumber[] {
-  if (Array.isArray(n))
-    return n.map(v => _clamp(v, min, max))
-
-  else
-    return _clamp(n, min, max)
+export function clamp(n: number, min: number, max: number): number {
+  return Math.max(Math.min(n, max), min)
 }
 
 
 
-function _clamp<T extends MaybeNumber>(n: T, min: number, max: number): T extends number ? number : number | undefined {
-  if (Number.isNaN(n) || isNil(n))
-    return undefined as any
-
-  else
-    return Math.max(Math.min(n, max), min)
+/** clamp Array version */
+export function clampArray<T extends MaybeNumber[]>(valueArr: T, min: number, max: number): T {
+  return valueArr.map(v => {
+    if (Number.isNaN(v) || isNil(v))
+      return undefined
+    else
+      return Math.max(Math.min(v, max), min)
+  }) as T
 }
-
-
