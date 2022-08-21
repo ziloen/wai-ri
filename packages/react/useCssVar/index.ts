aad9f251d6c86c67b1b040af464c27c4f1d1cb21
Neo <neo@neo.neo>
2022-08-21 13:47:33 +0800

    chore

--- packages/react/useCssVar/index.ts
+++ packages/react/useCssVar/index.ts
@@ -7,20 +7,21 @@ import { ref } from '../ref'
 type StyleElement = SVGElement | HTMLElement | MathMLElement
 type MaybeStyleElementRef = StyleElement | RefObject<StyleElement>
 
-export function useCssVar(propName: string, target: MaybeStyleElementRef, initVal = '') {
+export function useCssVar(propName: string, target?: MaybeStyleElementRef, initVal = '') {
   const cssVarRef = ref(initVal)
 
   useEffect(() => {
-    if (!target) return
-    const el = target instanceof Element ? target : target.current
-    if (!el) return
+    const el = target
+      ? target instanceof Element
+        ? target
+        : target.current
+      : window.document.documentElement
+
+    if (!el?.style) return
     // 动态target? 动态propName?
 
     el.style.setProperty(propName, cssVarRef.value)
 
-    return () => {
-      el.style.removeProperty(propName)
-    }
   }, [cssVarRef, target])
 
   return cssVarRef
