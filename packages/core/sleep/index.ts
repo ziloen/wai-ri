231ffe682f7cf4604375248f955fdda9cc32f409
N <34268416+ziloen@users.noreply.github.com>
2022-09-23 00:22:43 +0800

    chore

--- packages/core/sleep/index.ts
+++ packages/core/sleep/index.ts
@@ -2,7 +2,12 @@ import type { Fn } from '@wai-ri/shared'
 
 
 
-/** 延迟执行 */
-export function sleep<P extends any[], R = void>(ms?: number, func?: Fn<P, R>, ...args: P): Promise<R> {
-  return new Promise(res => setTimeout(() => res(func?.(...args) as R), ms))
+/** 等待 0 毫秒 */
+export function sleep(): Promise<void>
+/** 等待`ms`毫秒 */
+export function sleep(ms: number): Promise<void>
+/** 等待`ms`毫秒后执行函数 */
+export function sleep<P extends any[], R = void>(ms: number, fn: Fn<P, R>, ...args: P): Promise<R>
+export function sleep<P extends any[], R = void>(ms?: number, fn?: Fn<P, R>, ...args: P): Promise<R | void> {
+  return new Promise(res => setTimeout(() => res(fn?.(...args)), ms))
 }
