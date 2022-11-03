26465cdd698ccdfb9aed72da396dc8db1215cc41
N <34268416+ziloen@users.noreply.github.com>
2022-11-03 11:06:44 +0800

    feat: add EnumString util type

--- packages/shared/type/index.ts
+++ packages/shared/type/index.ts
@@ -51,6 +51,16 @@ export type AsyncFn<Args extends any[] = any[], Return = any> = (...args: Args) 
 export type ArrayType<A extends any[]> = A[number]
 
 
+/**
+ * 用于提示指定枚举字符串，但是允许 string 类型
+ * ```ts
+ * type EventName = EnumAndString<'click' | 'keydown' | 'keyup'>
+ * const e: EventName = "mouseup" // 输入时会有提示，且允许任意字符串
+ * ```
+ */
+export type EnumString<T> = T | (string & {})
+
+
 
 /** T 类型 L 长度的 Tuple */
 // export type FixedArray<T, L extends number, A extends unknown[] = []> = A['length'] extends L ? A : FixedArray<T, L, [T, ...A]>
@@ -125,20 +135,20 @@ export type ExpandDeep<
   // 已到达深度，结束
   Deep extends TargetDeep ? T :
   // 展开函数类型
-    T extends (...args: infer Params) => infer Return ? (...args: ExpandDeep<Params, TargetDeep, NextIter>) => ExpandDeep<Return, TargetDeep, NextIter> :
-    // 不展开 Promise 类型
-      T extends Promise<infer P> ? Promise<ExpandDeep<P, TargetDeep, NextIter>> :
-      // 展开对象类型
-        T extends object ? { [K in keyof T]: ExpandDeep<T[K], TargetDeep, NextIter> } :
-        // 展开联合类型
-          IsUnion<T> extends true ? ExpandDeep<ToTuple<T>[number], TargetDeep, NextIter> :
-          // 已完全展开，返回
-            T
+  T extends (...args: infer Params) => infer Return ? (...args: ExpandDeep<Params, TargetDeep, NextIter>) => ExpandDeep<Return, TargetDeep, NextIter> :
+  // 不展开 Promise 类型
+  T extends Promise<infer P> ? Promise<ExpandDeep<P, TargetDeep, NextIter>> :
+  // 展开对象类型
+  T extends object ? { [K in keyof T]: ExpandDeep<T[K], TargetDeep, NextIter> } :
+  // 展开联合类型
+  IsUnion<T> extends true ? ExpandDeep<ToTuple<T>[number], TargetDeep, NextIter> :
+  // 已完全展开，返回
+  T
 
 
 
 /** 用 New 类型 扩展 Org 类型*/
-export type Assign<Org extends ObjectType, New extends ObjectType> = Simplify< New & Omit<Org, keyof New>>
+export type Assign<Org extends ObjectType, New extends ObjectType> = Simplify<New & Omit<Org, keyof New>>
 
 /** 同 Assign */
 export type Merge<Org extends ObjectType, New extends ObjectType> = Simplify<New & Omit<Org, keyof New>>
@@ -153,13 +163,13 @@ export type Extensible<O extends ObjectType> = Simplify<O & { [K: keyof any]: un
 /** 使两属性互斥，不是线程互斥锁，Disjoint / Mutex / MutuallyExclusive */
 export type Mutex<T, K1 extends keyof T, K2 extends keyof T> =
   Simplify<
-  (
-    { [K in Exclude<keyof T, K1 | K2>]: T[K] } &
     (
-      | { [K in K1]?: never } & { [K in K2]: T[K] }
-      | { [K in K2]?: never } & { [K in K1]: T[K] }
+      { [K in Exclude<keyof T, K1 | K2>]: T[K] } &
+      (
+        | { [K in K1]?: never } & { [K in K2]: T[K] }
+        | { [K in K2]?: never } & { [K in K1]: T[K] }
+      )
     )
-  )
   >
 // | ({ [P in Exclude<K, K1>]: T[P] } & { [P in K1]?: never })
 // | ({ [P in Exclude<K, K2>]: T[P] } & { [P in K2]?: never })
@@ -196,10 +206,10 @@ export type SetRequired<T, K extends keyof T> = RequiredByKeys<T, K>
 export type CombineObjectUnion<T> = {
   [K in (T extends infer P ? keyof P : never)]:
   T extends infer P
-    ? K extends keyof P
-      ? P[K]
-      : never
-    : never
+  ? K extends keyof P
+  ? P[K]
+  : never
+  : never
 }
 
 
@@ -215,7 +225,7 @@ export type CombineObjectUnion<T> = {
  */
 export type ExtractByKeys<T, K extends keyof any> =
   T extends infer R
-    ? K extends keyof R
-      ? R
-      : never
-    : never
+  ? K extends keyof R
+  ? R
+  : never
+  : never
