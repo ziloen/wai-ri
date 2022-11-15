0c59669422b1bef98814f63ffb6aa80147adb284
N <34268416+ziloen@users.noreply.github.com>
2022-11-15 17:11:43 +0800

    feat: add MaybeEmpty

--- packages/shared/type/index.ts
+++ packages/shared/type/index.ts
@@ -33,7 +33,7 @@ export type ObjectType<K extends keyof any = keyof any, V = any> = Record<K, V>
 
 /** 空对象类型 */
 declare const emptyObjectSymbol: unique symbol
-export type EmptyObject = { [emptyObjectSymbol]?: never }
+export interface EmptyObject { [emptyObjectSymbol]?: never }
 
 
 
@@ -60,6 +60,11 @@ export type ArrayType<A extends any[]> = A[number]
  */
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type EnumString<T> = T | (string & {})
+
+
+
+/** 要么是空对象，要么是指定类型 (使当前对象与空对象互斥，相比Partial，只要有一个属性存在，则其余属性皆存在) */
+export type MaybeEmpty<T> = T | { [K in keyof T]: undefined }
 
 
 
@@ -127,17 +132,17 @@ export type Expand<T> = ExpandDeep<T, 3>
 
 type StopExpandIfRecursion<T, P, TargetDeep extends number, NextIter extends any[]> =
   Equal<T, P> extends true
-  ? P
-  : Equal<T[], P> extends true
-  ? P
-  : ExpandDeep<P, TargetDeep, NextIter>
+    ? P
+    : Equal<T[], P> extends true
+      ? P
+      : ExpandDeep<P, TargetDeep, NextIter>
 
 
 /** 获取所有可选属性 */
 export type OptionalKeysOf<T> = Exclude<{
   [K in keyof T]: T extends Record<K, T[K]>
-  ? never
-  : K
+    ? never
+    : K
 }[keyof T], undefined>
 
 
@@ -161,15 +166,15 @@ export type ExpandDeep<
   // 已到达深度，结束
   Deep extends TargetDeep ? T :
   // 展开函数类型
-  T extends (...args: infer Params) => infer Return ? (...args: ExpandDeep<Params, TargetDeep, NextIter>) => StopExpandIfRecursion<T, Return, TargetDeep, NextIter> :
-  // 不展开 Promise 类型
-  T extends Promise<infer P> ? Equal<T, P> extends true ? T : Promise<ExpandDeep<P, TargetDeep, NextIter>> :
-  // 展开对象类型
-  T extends object ? { [K in keyof T]: StopExpandIfRecursion<T, T[K], TargetDeep, NextIter> } :
-  // 展开联合类型
-  IsUnion<T> extends true ? ExpandDeep<ToTuple<T>[number], TargetDeep, NextIter> :
-  // 已完全展开，返回
-  T
+    T extends (...args: infer Params) => infer Return ? (...args: ExpandDeep<Params, TargetDeep, NextIter>) => StopExpandIfRecursion<T, Return, TargetDeep, NextIter> :
+    // 不展开 Promise 类型
+      T extends Promise<infer P> ? Equal<T, P> extends true ? T : Promise<ExpandDeep<P, TargetDeep, NextIter>> :
+      // 展开对象类型
+        T extends object ? { [K in keyof T]: StopExpandIfRecursion<T, T[K], TargetDeep, NextIter> } :
+        // 展开联合类型
+          IsUnion<T> extends true ? ExpandDeep<ToTuple<T>[number], TargetDeep, NextIter> :
+          // 已完全展开，返回
+            T
 
 
 
@@ -182,18 +187,18 @@ export type Merge<Org extends ObjectType, New extends ObjectType> = Simplify<New
 
 
 /** 可扩展类型，在添加未约束的属性时不会报错 */
-export type Extensible<O extends ObjectType> = Simplify<O & { [K: keyof any]: unknown }>
+export type Extensible<O extends ObjectType> = Simplify<O & Record<keyof any, unknown>>
 
 
 
 /** 使两属性互斥，不是线程互斥锁，Disjoint / Mutex / MutuallyExclusive */
 export type Mutex<T, K1 extends keyof T, K2 extends keyof T> =
   Simplify<
-    { [K in Exclude<keyof T, K1 | K2>]: T[K] } &
-    (
-      | { [K in K1]?: never } & { [K in K2]: T[K] }
-      | { [K in K2]?: never } & { [K in K1]: T[K] }
-    )
+  { [K in Exclude<keyof T, K1 | K2>]: T[K] } &
+  (
+    | { [K in K1]?: never } & { [K in K2]: T[K] }
+    | { [K in K2]?: never } & { [K in K1]: T[K] }
+  )
   >
 // | ({ [P in Exclude<K, K1>]: T[P] } & { [P in K1]?: never })
 // | ({ [P in Exclude<K, K2>]: T[P] } & { [P in K2]?: never })
@@ -230,10 +235,10 @@ export type SetRequired<T, K extends keyof T> = RequiredByKeys<T, K>
 export type CombineObjectUnion<T> = {
   [K in (T extends infer P ? keyof P : never)]:
   T extends infer P
-  ? K extends keyof P
-  ? P[K]
-  : never
-  : never
+    ? K extends keyof P
+      ? P[K]
+      : never
+    : never
 }
 
 
@@ -249,7 +254,7 @@ export type CombineObjectUnion<T> = {
  */
 export type ExtractByKeys<T, K extends keyof any> =
   T extends infer R
-  ? K extends keyof R
-  ? R
-  : never
-  : never
+    ? K extends keyof R
+      ? R
+      : never
+    : never
