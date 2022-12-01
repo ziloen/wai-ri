c35d12941f969e192ed563d307553f8e0fe7eb71
N <34268416+ziloen@users.noreply.github.com>
2022-12-01 12:36:30 +0800

    chore

--- packages/react/hooks/rxjs/index.ts
+++ packages/react/hooks/rxjs/index.ts
@@ -1,8 +1,8 @@
 // 参考 rxooks
 
-import { useEffect, useMemo, useState } from 'react'
-import type{ Subscription } from 'rxjs'
-import { BehaviorSubject } from 'rxjs'
+import { noop } from '@wai-ri/core'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { BehaviorSubject, Subscription, exhaustMap, fromEvent, Observable } from 'rxjs'
 import { VueRef } from '../../useVueRef'
 
 
@@ -35,7 +35,36 @@ export function useBehaviorSubjectRef<T>(subject: BehaviorSubject<T>): VueRef<T>
 }
 
 
-/** 结束时自动退订 */
-export function useSubscription(sub: Subscription) {
-  useEffect(() => () => sub.unsubscribe())
+/**
+ * 结束时自动退订
+ * ```ts
+ * useSubscription(() =>
+ *   click$
+ *     .pipe(
+ *       map(e => e.x),
+ *       exhaustMap(x => fetch(...))
+ *     )
+ * ).subscribe(result => setState(result))
+ * ```
+ */
+export function useSubscription<T>(fn: () => Observable<T>) {
+  const ob = fn()
+
+  // const subscription = useRef<Subscription | undefined>()
+
+  // eslint-disable-next-line @typescript-eslint/no-extra-parens
+  const subscription = useRef<Partial<Observable<T>> | ((value: T) => void)>(noop)
+
+  useEffect(() => {
+    const sub = ob.subscribe((...args) => {
+      subscription.current
+    })
+    return () => sub.unsubscribe()
+  }, [])
+
+  return {
+    subscribe: (observer?: Partial<Observable<T>> | ((value: T) => void)) => {
+
+    }
+  }
 }
