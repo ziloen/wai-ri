8c6e35fd87c1f6a5a24f69d1589a881cd2bfeeab
N <34268416+ziloen@users.noreply.github.com>
2023-01-26 01:55:09 +0800

    chore: update deps

--- packages/rxios/useRxios/index.ts
+++ packages/rxios/useRxios/index.ts
@@ -8,6 +8,7 @@ export function useRxios(instance: AxiosInstance) {
   return function rxios<ResponseData, ConfigData>(config: AxiosRequestConfig<ConfigData>) {
     return new Observable<AxiosResponse<ResponseData, ConfigData>>(observer => {
       const controller = new AbortController
+
       instance.request<ResponseData>({ ...config, signal: controller.signal })
         .then(val => (observer.next(val), observer.complete()))
         .catch(err => AxiosStatic.isCancel(err) ? observer.complete() : observer.error(err))
