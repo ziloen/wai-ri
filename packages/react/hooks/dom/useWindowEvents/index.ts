905df04cce69846ac0db7856371dee83f6b9a3ab
N <34268416+ziloen@users.noreply.github.com>
2023-02-18 03:18:11 +0800

    chore: add device pixel ratio change hook

--- packages/react/hooks/dom/useWindowEvents/index.ts
+++ packages/react/hooks/dom/useWindowEvents/index.ts
@@ -1,37 +1,100 @@
 import { useLatest } from '@wai-ri/react'
 import { useEffect } from 'react'
 import { fromEvent, Observable } from 'rxjs'
-import { share } from 'rxjs/operators'
+import { map, share } from 'rxjs/operators'
 
 
-function craeteSharedSubscribeHook<T>(observerable: Observable<T>) {
-  const shareable$ = observerable.pipe(share({ resetOnRefCountZero: true }))
-
-  return function (fn: (value: T) => void) {
+/** 创建订阅 hook，卸载时自动取消订阅 */
+function craeteSubscribeHook<V>(observerable: Observable<V>) {
+  return function (fn: (value: V) => void) {
     const fnRef = useLatest(fn)
     useEffect(() => {
-      const subscription = shareable$.subscribe(e => fnRef.current(e))
+      const subscription = observerable.subscribe(v => fnRef.current(v))
       return () => subscription.unsubscribe()
     }, [])
   }
 }
 
-export const useWindowKeyDown = craeteSharedSubscribeHook(
-  fromEvent<KeyboardEvent>(window, 'keydown', { passive: true, capture: true })
+/** window 按键抬起事件 */
+export const useWindowKeyDown = craeteSubscribeHook(
+  fromEvent<KeyboardEvent>(window, 'keydown', {
+    passive: true
+  }).pipe(share({ resetOnRefCountZero: true }))
 )
 
-export const useWindowKeyup = craeteSharedSubscribeHook(
-  fromEvent<KeyboardEvent>(window, 'keyup', { passive: true, capture: true })
+
+/** window 按键抬起事件 */
+export const useWindowKeyup = craeteSubscribeHook(
+  fromEvent<KeyboardEvent>(window, 'keyup', {
+    passive: true
+  }).pipe(share({ resetOnRefCountZero: true }))
 )
 
-export const useWindowScroll = craeteSharedSubscribeHook(
-  fromEvent(window, 'scroll', { passive: true, capture: true })
+/** window 滚动监听 */
+export const useWindowScroll = craeteSubscribeHook(
+  fromEvent(window, 'scroll', { passive: true }).pipe(
+    share({ resetOnRefCountZero: true })
+  )
 )
 
-export const useWindowResize = craeteSharedSubscribeHook(
-  fromEvent<UIEvent>(window, 'resize', { passive: true, capture: true })
+/** window 尺寸调整监听 */
+export const useWindowResize = craeteSubscribeHook(
+  fromEvent<UIEvent>(window, 'resize', { passive: true }).pipe(
+    share({ resetOnRefCountZero: true })
+  )
 )
 
-export const useWindowPointer = craeteSharedSubscribeHook(
+// 🚧
+const useWindowPointer = craeteSubscribeHook(
   fromEvent(window, 'pointermove', { passive: true, capture: true })
+    .pipe(
+      share({ resetOnRefCountZero: true })
+    )
 )
+
+/** 页面可见性改变事件流 */
+const visibilityChange$ = fromEvent(document, 'visibilitychange', {
+  passive: true
+}).pipe(
+  share({ resetOnRefCountZero: true }),
+  map(() => document.visibilityState !== 'hidden')
+)
+
+/** 页面可见性监听 */
+export const useVisibleChange = craeteSubscribeHook(visibilityChange$)
+
+/** 监听设备像素比变化 */
+export function useDevicePixelRatioChange(fn: (dpr: number) => void) {
+  const fnRef = useLatest(fn)
+  useEffect(() => {
+    const ac = new AbortController
+    function update() {
+      matchMedia(
+        `(resolution: ${window.devicePixelRatio}dppx)`
+      ).addEventListener(
+        'change',
+        () => (fnRef.current(window.devicePixelRatio), update()),
+        { once: true, signal: ac.signal }
+      )
+    }
+    update()
+    return () => ac.abort()
+  }, [])
+}
+
+// 另一种实现，监听 window resize 事件，映射为像素比，并过滤重复事件
+// /** 设备像素比变化事件流 */
+// const devicePixelRatioChange$ = fromEvent<UIEvent>(window, "resize", {
+//   passive: true
+// })
+//   .pipe(share({ resetOnRefCountZero: true }))
+//   .pipe(
+//     // 映射为 设备像素比
+//     map(() => window.devicePixelRatio),
+//     // 忽略直到变化前
+//     distinctUntilChanged()
+//   );
+// /** 设备像素比变化监听 */
+// export const useDevicePixelRatioChange = craeteSubscribeHook(
+//   devicePixelRatioChange$.pipe(share({ resetOnRefCountZero: true }))
+// );
