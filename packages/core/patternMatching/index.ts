7f6afd6c576e8995a891296b23a315a340e38b95
N <34268416+ziloen@users.noreply.github.com>
2023-08-31 16:17:59 +0800

    chore: update patternmatching

--- packages/core/patternMatching/index.ts
+++ packages/core/patternMatching/index.ts
@@ -1,3 +1,5 @@
+import { isFn } from '@wai-ri/shared'
+
 const FALLBACK_ARM = '_'
 
 type GetMatchArmResults<
@@ -12,6 +14,17 @@ type GetMatchArmResults<
 /**
  * 模式匹配
  * {@link FALLBACK_ARM default 分支}
+ * @example
+ * ```ts
+ * const value = "a" as unknown
+ * 
+ * //    ↓ 'a' | 'b' | 'c'
+ * const result = patternMatching(value, {
+ *   a: () => 'a',
+ *   b: "b",
+ *   _: "c"
+ * })
+ * ```
  */
 export function patternMatching<
   V,
@@ -21,6 +34,12 @@ export function patternMatching<
   value: K,
   matchArms: T
 ): GetMatchArmResults<T> {
-  const resultArm = value in matchArms ? matchArms[value] : matchArms[FALLBACK_ARM as K]
-  return (typeof resultArm === 'function' ? resultArm() : resultArm) as GetMatchArmResults<T>
+  const resultArm = Object.hasOwn(matchArms, value)
+    ? matchArms[value]
+    : matchArms[FALLBACK_ARM as K]
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return isFn(resultArm)
+    ? resultArm()
+    : resultArm
 }
